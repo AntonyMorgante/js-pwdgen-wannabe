a63c5186ffbb5f0c8d33a72e3990ab8bef7df3a4
AntonyMorgante <antonymorgante1@gmail.com>
2021-12-02 14:45:13 +0100

    script

---
+++ js/myscript.js
@@ -0,0 +1,12 @@
+/* Nome cognome colore */
+
+const user_name = prompt("Inserisci il tuo nome");
+const user_surname = prompt("Inserisci il tuo cognome");
+const user_favcolour = prompt("Inserisci il tuo colore preferito");
+
+
+
+document.writeln(
+    `Salve, ${user_name} ${user_surname}. Alla domanda sul tuo colore preferito hai risposto ${user_favcolour}. Puoi procedere.`
+)
+
